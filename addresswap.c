442ecff392282202d935b7b0b4baac1b459c353d
Soujanya-R-N <rnaiksoujanya@gmail.com>
2025-05-06 19:16:06 +0530

    Add files via upload

---
+++ addresswap.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+   int a=10,b=20;
+   int *ptra=&a;
+   int *ptrb=&b;
+   int size;
+   char var[size];
+   printf("%d\n",ptra);
+   scanf("%c",&var);
+    printf("%d\n",ptrb);
+
+    int length=strlen(ptra);
+    printf("%d",length);
+    int charr[length];
+}
+int swap(int *pbra,int *pbrb,int n,int length)
+{
+    int temp=0;
+    for(int i=0; i<length;i++)
+    {
+
+    }
+}
+
